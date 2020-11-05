02ba42b2f1f18e399c8c2190ced095f59531c67b
Jason Liebault <jason.liebault@simsoft-industry.fr>
2020-11-05 12:47:39 +0100

    Add CameraPreview + AndroidX plugin support + android permissions plugin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,16 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { CameraPreview } from '@ionic-native/camera-preview';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,7 +25,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    CameraPreview,
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CameraPreview, CameraPreviewOptions } from '@ionic-native/camera-preview';
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -6,9 +7,35 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  text: any;
+  picture: string;
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    cameraPreview: CameraPreview,
+  ) {
+    const cameraPreviewOpts: CameraPreviewOptions = {
+      x: 0,
+      y: 0,
+      width: window.screen.width,
+      height: window.screen.height,
+      camera: "rear",
+      tapPhoto: true,
+      previewDrag: true,
+      toBack: false,
+      alpha: 1,
+    };
 
+    cameraPreview.startCamera(cameraPreviewOpts).then(
+      (res) => {
+        this.text = res;
+        console.log(res);
+      },
+      (err) => {
+        this.text = err;
+        console.log(err);
+      }
+    );
   }
 
 }
