dc5e7698ae6dcfb048857ae8185e2c72b9bf9119
Mathieu Pages <pagesmat@gmail.com>
2020-11-26 14:18:56 +0100

    merge Jason & Mathieu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
-<<<<<<< HEAD
 import { ErrorHandler, NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
-=======
-import { CUSTOM_ELEMENTS_SCHEMA, ErrorHandler, NgModule } from '@angular/core';
->>>>>>> 4e94c9913749f6aaeac970ebc597812b1fc9a8f2
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ComponentsModule } from '../components/components.module';
-<<<<<<< HEAD
+import { SearchService } from '../services/search/search.service';
 import { PhotoService } from '../services/photo.service';
 import { IonicStorageModule } from '@ionic/storage';
-=======
-import { SearchService } from '../services/search/search.service';
 import { HttpClientModule } from '@angular/common/http';
 import { UtilsHttpService } from '../services/http/utils.http.service';
->>>>>>> 4e94c9913749f6aaeac970ebc597812b1fc9a8f2
 
 @NgModule({
   declarations: [
@@ -25,7 +18,7 @@ import { UtilsHttpService } from '../services/http/utils.http.service';
     HomePage,
   ],
   imports: [
-    BrowserModule,        
+    BrowserModule,
     HttpClientModule,
     IonicModule.forRoot(MyApp),
     IonicStorageModule.forRoot(),
@@ -40,14 +33,10 @@ import { UtilsHttpService } from '../services/http/utils.http.service';
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
-<<<<<<< HEAD
-    PhotoService,
-  ],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
-=======
     SearchService,
+    PhotoService,
     UtilsHttpService
   ],
->>>>>>> 4e94c9913749f6aaeac970ebc597812b1fc9a8f2
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule {}
