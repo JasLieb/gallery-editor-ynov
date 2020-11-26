b45de43705652f155f0e63baf73a7fe946ed5913
Nummy <puel.quentin81@gmail.com>
2020-11-26 11:26:18 +0100

    Ajout de multiple bouton et leurs comportements

--- src/components/imageview/imageview.ts
+++ src/components/imageview/imageview.ts
@@ -1,35 +1,63 @@
 import { Component, ViewChild,ElementRef, OnInit } from '@angular/core';
 import { getQueryValue } from '@angular/core/src/view/query';
 
-/**
- * Generated class for the ImageviewComponent component.
- *
- * See https://angular.io/api/core/Component for more info on Angular
- * Components.
- */
+interface FilterOptions {
+  grey: boolean;
+  cep: boolean;
+  flou: boolean;
+}
+
 @Component({
   selector: 'imageview',
   templateUrl: 'imageview.html'
 })
 export class ImageviewComponent {
-  status: boolean = false;
-  cep: boolean = false;
-  flou: boolean = false;
+  private options: FilterOptions;
+  imageFilterStyle: string = '';
+
   constructor() {
     console.log('Hello ImageviewComponent Component');
-    
-    
-    
+    this.options = {
+      grey: false, 
+      cep: false, 
+      flou: false
+    } as FilterOptions;
   }
-  
+
+  formatFilterOptions(): object {
+    const greyClass = this.options.grey
+      ? 'grayscale(1)'
+      : 'grayscale(0)';
+
+    const cepClass = this.options.cep
+      ? 'sepia(1)'
+      : 'sepia(0)';
+
+    const flouClass = this.options.flou
+      ? 'blur(2px)'
+      : 'blur(0)';
+
+    return {'filter': `${greyClass} ${cepClass} ${flouClass}`};
+  }
   
   applyfilter(){
-    this.status = !this.status;
+    this.options = {
+      ...this.options,
+      grey: !this.options.grey
+    }
   }
+
   applyfilterCep(){
-    this.cep = !this.cep
+    this.options = {
+      ...this.options,
+      cep: !this.options.cep
+    }
   }
+
   applyfilterFlou(){
-    this.flou = !this.flou
+    this.options = {
+      ...this.options,
+      flou: !this.options.flou
+    }
   }
 }
