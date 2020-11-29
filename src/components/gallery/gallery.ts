31e35be778894fa3dd5db3d8d74bbabc6b0430c9
Jason Liebault <jason.liebault@simsoft-industry.fr>
2020-11-29 16:22:25 +0100

    WIP

--- src/components/gallery/gallery.ts
+++ src/components/gallery/gallery.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { SearchService } from '../../services/search/search.service';
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+import { BehaviorSubject, Observable } from "rxjs";
+import { SearchService } from "../../services/search/search.service";
 import { imagesPaths } from "../../assets/imgs/imageDictionary";
-import { DateInterval, SearchOptions } from '../../core/model/filters/searchOptions.model';
-import { Localization } from '../../core/model/filters/localization.model';
-import { SearchFilters } from '../../core/model/filters/searchFilters.model';
-import { Device } from '../../core/model/filters/device.model';
+
+import { Localization } from "../../core/model/filters/localization.model";
+import { SearchFilters } from "../../core/model/filters/searchFilters.model";
+import { Device } from "../../core/model/filters/device.model";
 
 interface IonicDatetimeValue {
   year: number;
@@ -14,11 +14,13 @@ interface IonicDatetimeValue {
 }
 
 @Component({
-  selector: 'gallery',
-  templateUrl: 'gallery.html'
+  selector: "gallery",
+  templateUrl: "gallery.html",
 })
 export class GalleryComponent implements OnInit {
+  @ViewChild("canvasEl") canvas: ElementRef;
   cameraOnOffBehavior: BehaviorSubject<boolean>;
+  selectedImagesBehavior: BehaviorSubject<string[]>;
   filters$: Observable<SearchFilters>;
   images: string[];
 
@@ -28,16 +30,21 @@ export class GalleryComponent implements OnInit {
   defaultDevice = Device.default;
   selectedDevice: Device = this.defaultDevice;
 
-  constructor(
-    private searchService: SearchService
-  ) { 
+  constructor(private searchService: SearchService) {
     this.cameraOnOffBehavior = new BehaviorSubject(false);
-    this.images  = [...imagesPaths];
+    this.selectedImagesBehavior = new BehaviorSubject([]);
+    this.images = [...imagesPaths];
     this.searchService.loadMetadata(this.images);
     this.filters$ = this.searchService.availableFiltersBehavior.asObservable();
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (this.canvas) {
+      this.addImageToPatchwork(this.images[0], 0, 0, 200, 200);
+      this.addImageToPatchwork(this.images[4], 200, 0, 100, 100);
+      this.addImageToPatchwork(this.images[1], 200, 100, 100, 100);
+    }
+  }
 
   handleOnOffCamera(on: boolean) {
     this.cameraOnOffBehavior.next(on);
@@ -47,17 +54,63 @@ export class GalleryComponent implements OnInit {
     this.isSearchOpen = true;
   }
 
+  selectImage(image: string) {
+    const currentSelected = this.selectedImagesBehavior.value;
+    let selectedImages = [];
+
+    if(currentSelected.includes(image)) {
+      selectedImages = currentSelected.filter(i => i !== image)
+    } else {
+      selectedImages = [
+        ...currentSelected,
+        image
+      ].filter(
+        (value, index, self) => self.indexOf(value) === index
+      );
+    }
+      
+    this.selectedImagesBehavior.next(selectedImages);
+  }
+
+  isSelectedImage(image: string) : boolean {
+    return this.selectedImagesBehavior.value.includes(image);
+  }
+
   makeSearch(minDate: IonicDatetimeValue, maxDate: IonicDatetimeValue) {
     const dateInterval = {
-      min: minDate.year ? new Date(minDate.year, minDate.month, minDate.day) : null,
-      max: maxDate.year ? new Date(maxDate.year, maxDate.month, maxDate.day) : null
+      min: minDate.year
+        ? new Date(minDate.year, minDate.month, minDate.day)
+        : null,
+      max: maxDate.year
+        ? new Date(maxDate.year, maxDate.month, maxDate.day)
+        : null,
     };
-    
+
     this.images = this.searchService.filter({
-        localization: this.selectedLocalization,
-        device: this.selectedDevice,
-        dateInterval
+      localization: this.selectedLocalization,
+      device: this.selectedDevice,
+      dateInterval,
     });
     this.isSearchOpen = false;
+  }
+
+  private addImageToPatchwork(imageSource, x, y, width, height) {
+    const context = (this.canvas.nativeElement as HTMLCanvasElement).getContext("2d");
+
+    var img = new Image(); 
+    img.src = imageSource;
+
+    img.onload = () => {
+
+      context.drawImage(
+        img,
+        // 0,0, // Start at (x, y) of the image (crop)
+        // width, height, // "Get" a w * h area from the source image (crop)
+        x, y,  // Place the result at (x, y) in the canvas,
+        width, height  // With as width * height
+      );
+
+      console.log(context);
+    }; 
   }
 }
